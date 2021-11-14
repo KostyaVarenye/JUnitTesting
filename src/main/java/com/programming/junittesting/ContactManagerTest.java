c61b745a9fff4b21a75ef61dc923ec87e450aec8
Kostya <kovevare@gmail.com>
2021-11-14 08:40:22 +0200

    created fist test for shouldCreateContact, check if not empty on creation, check exactly 1

--- src/main/java/com/programming/junittesting/ContactManagerTest.java
+++ src/main/java/com/programming/junittesting/ContactManagerTest.java
@@ -1,5 +1,6 @@
 package com.programming.junittesting;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class ContactManagerTest {
     @Test
     public void shouldCreateContact(){
-
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact("John", "Doe", "0123456789");
+        // test if the method is not empty, if contact created
+        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+        // expected to have exactly 1 contact
+        Assertions.assertEquals(1, contactManager.getAllContacts().size());
     }
 }
